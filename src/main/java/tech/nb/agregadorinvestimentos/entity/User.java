acce4f6107fab9dcb63e572a2ed854f8a08c6ce9
Nicolas Bueno <nicolasbrian202@gmail.com>
2024-10-26 15:16:12 -0300

    feat: Create Entity ‘User’

---
+++ src/main/java/tech/nb/agregadorinvestimentos/entity/User.java
@@ -0,0 +1,38 @@
+package tech.nb.agregadorinvestimentos.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb-users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+}
